3f7cbe009e66e02f17da929e6cf3a15cbc6bc6ff
jcsgirish <jcsgirish7@gmail.com>
2024-06-26 23:27:15 +0530

    Deployed

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,11 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: 'https://your-frontend-domain.vercel.app',
+  methods: 'GET,POST,PUT,DELETE,PATCH',
+  credentials: true // if you need to include cookies in requests
+}));
 
 
 
